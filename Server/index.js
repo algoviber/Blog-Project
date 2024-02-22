e19d8c35363ccddefa9d2fbb82e7a6438255b84a
algoviber <149084047+algoviber@users.noreply.github.com>
2024-02-23 04:19:20 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -17,13 +17,13 @@ const Post = require('./models/Post');
 
 app.use(cors({credentials: true,origin:'http://localhost:3000'}));
 
-app.use(cors(
-    {
-        origin: [""],
-        methods;["POST","GET"],
-        credentials: true
-}
-));
+// app.use(cors(
+//     {
+//         origin: [""],
+//         methods;["POST","GET"],
+//         credentials: true
+// }
+// ));
 
 app.use(express.json());
 app.use(cookieParser());
