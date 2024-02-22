2b31439666b684e072aebf4fa356dfd37d73c926
algoviber <149084047+algoviber@users.noreply.github.com>
2024-02-23 04:09:51 +0530

    Update Header.js

--- Frontend/src/Header.js
+++ Frontend/src/Header.js
@@ -5,7 +5,7 @@ export default function Header(){
   
   const {setUserInfo,userInfo}= useContext(UserContext);
   useEffect(() => {
-    fetch('http://localhost:4000/profile',{
+    fetch('https://blog-project-five-rho.vercel.app/profile',{
       credentials: 'include',
 
     }).then(response=>{
@@ -16,7 +16,7 @@ export default function Header(){
   },[]);
 
   function logout(){
-    fetch('http://localhost:4000/logout',{
+    fetch('https://blog-project-five-rho.vercel.app/logout',{
       credentials: 'include',
       method: 'POST',
     });
