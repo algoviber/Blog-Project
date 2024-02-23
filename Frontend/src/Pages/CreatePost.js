d0f67e3f4af97ff14c0d821c2117fa56051b1398
algoviber <149084047+algoviber@users.noreply.github.com>
2024-02-24 04:47:52 +0530

    Update CreatePost.js

--- Frontend/src/Pages/CreatePost.js
+++ Frontend/src/Pages/CreatePost.js
@@ -20,7 +20,7 @@ export default function CreatePost(){
 
         ev.preventDefault();
         
-        const response = await fetch('https://blog-api-back.vercel.app/post',{
+        const response = await fetch('https://api-psi-rouge.vercel.app/post',{
            method: 'POST',
             body: data,
             credentials: 'include',
