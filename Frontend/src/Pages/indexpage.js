e8be2b5302f393b393267604fe328892f20c39d3
algoviber <149084047+algoviber@users.noreply.github.com>
2024-02-23 05:37:29 +0530

    Update indexpage.js

--- Frontend/src/Pages/indexpage.js
+++ Frontend/src/Pages/indexpage.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 export default function Indexpage(){
     const[posts,setPosts]=useState([]);
     useEffect(()=>{
-        fetch('https://blog-project-five-rho.vercel.app/post').then(response=>{
+        fetch('http://localhost:4000/post').then(response=>{
             response.json().then(posts=>{
             setPosts(posts);
             });
