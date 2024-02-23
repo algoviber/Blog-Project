905289b43f8b4dd3dde8794bc592b120ffa985fc
algoviber <149084047+algoviber@users.noreply.github.com>
2024-02-23 05:30:34 +0530

    Update CreatePost.js

--- Frontend/src/Pages/CreatePost.js
+++ Frontend/src/Pages/CreatePost.js
@@ -20,7 +20,7 @@ export default function CreatePost(){
 
         ev.preventDefault();
         
-        const response = await fetch('https://blog-project-five-rho.vercel.app/post',{
+        const response = await fetch('http://localhost:4000/post',{
            method: 'POST',
             body: data,
             credentials: 'include',
