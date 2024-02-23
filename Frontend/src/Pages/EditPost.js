a211e0c8952f846bf7519e654148636745bf24fb
algoviber <149084047+algoviber@users.noreply.github.com>
2024-02-23 05:36:31 +0530

    Update EditPost.js

--- Frontend/src/Pages/EditPost.js
+++ Frontend/src/Pages/EditPost.js
@@ -13,7 +13,7 @@ export default function EditPost(){
     const[files,setFiles] = useState('');
     const[redirect,setRedirect]=useState(false);
     useEffect(()=>{
-        fetch('http://localhost:4000'+id)
+        fetch('http://localhost:4000/'+id)
             .then(response=>{
                 response.json().then(postInfo=>{
                     setTitle(postInfo.title);
