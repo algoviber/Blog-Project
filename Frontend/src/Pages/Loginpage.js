afb174ddb22dc10c240886cd5493f873493736ce
algoviber <149084047+algoviber@users.noreply.github.com>
2024-02-24 17:23:29 +0530

    Update Loginpage.js

--- Frontend/src/Pages/Loginpage.js
+++ Frontend/src/Pages/Loginpage.js
@@ -11,7 +11,7 @@ export default function Loginpage(){
 
     async function login(ev){
         ev.preventDefault();
-        const response = await fetch('https://api-drab-two.vercel.app/login',{
+        const response = await fetch('https://api-alpha-orcin-41.vercel.app/login',{
             method: 'POST',
             body: JSON.stringify({username,password}),
             headers: {'Content-Type':'application/json'},
