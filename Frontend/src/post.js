2aaee2e6a5ef4b8d5fe4aca5238cd195e5862ccc
algoviber <149084047+algoviber@users.noreply.github.com>
2024-02-24 17:21:14 +0530

    Update post.js

--- Frontend/src/post.js
+++ Frontend/src/post.js
@@ -6,7 +6,7 @@ export default function Post({_id,title,summary,cover,content,createdAt,author})
         <div className="post">
         <div className="image">
         <Link to={`/post/${_id}`}>
-          <img src={'https://api-drab-two.vercel.app/'+cover} alt="" />
+          <img src={'https://api-alpha-orcin-41.vercel.app/'+cover} alt="" />
         </Link>
         </div>
 
