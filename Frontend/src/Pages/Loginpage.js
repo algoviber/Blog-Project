f0bf5ddde2b209cba63f1f6d1d40ac0bb9a3458a
algoviber <149084047+algoviber@users.noreply.github.com>
2024-02-24 19:36:05 +0530

    Update Loginpage.js

--- Frontend/src/Pages/Loginpage.js
+++ Frontend/src/Pages/Loginpage.js
@@ -11,7 +11,7 @@ export default function Loginpage(){
 
     async function login(ev){
         ev.preventDefault();
-        const response = await fetch('https://api-alpha-orcin-41.vercel.app/login',{
+        const response = await fetch('https://api-nu-sable-23.vercel.app/login',{
             method: 'POST',
             body: JSON.stringify({username,password}),
             headers: {'Content-Type':'application/json'},
