1cf24cc97524695657116278b8a4430b4a11971d
algoviber <149084047+algoviber@users.noreply.github.com>
2024-02-27 00:42:22 +0530

    Update post.js

--- Frontend/src/post.js
+++ Frontend/src/post.js
@@ -6,7 +6,7 @@ export default function Post({_id,title,summary,cover,content,createdAt,author})
         <div className="post">
         <div className="image">
         <Link to={`/post/${_id}`}>
-          <img src={'https://blog-project-y3yj.onrender.com/'+cover} alt="" />
+          <img src={cover} alt="" />
         </Link>
         </div>
 
