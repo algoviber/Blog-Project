2c2791d890f90c3f37c9ff1ca2be86f817199fae
algoviber <149084047+algoviber@users.noreply.github.com>
2024-02-26 18:01:32 +0530

    Update post.js

--- Frontend/src/post.js
+++ Frontend/src/post.js
@@ -6,7 +6,7 @@ export default function Post({_id,title,summary,cover,content,createdAt,author})
         <div className="post">
         <div className="image">
         <Link to={`/post/${_id}`}>
-          <img src={'https://api-nu-sable-23.vercel.app/'+cover} alt="" />
+          <img src={'https://blog-project-y3yj.onrender.com/'+cover} alt="" />
         </Link>
         </div>
 
